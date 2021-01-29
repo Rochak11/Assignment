ec05a6dc1602b3f0359a656801b3fbafbb1092b9
asus <rochakpaudel34@gmail.com>
2021-01-29 19:37:34 +0545

    commit

--- app/src/main/java/com/example/login/PermissionActivity.kt
+++ app/src/main/java/com/example/login/PermissionActivity.kt
@@ -1,7 +1,9 @@
 package com.example.login
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 
 class PermissionActivity : AppCompatActivity() {
     private val permissions = arrayOf(
@@ -12,5 +14,29 @@ class PermissionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_permission)
+        // Check for permission
+        if (!hasPermission()) {
+            requestPermission()
+        }
     }
-}
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+                this@PermissionActivity,
+                permissions, 1
+        )
+    }
+    private fun hasPermission(): Boolean {
+        var hasPermission = true
+        for (permission in permissions) {
+            if (ActivityCompat.checkSelfPermission(
+                            this,
+                            permission
+                    ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                hasPermission = false
+            }
+        }
+        return hasPermission
+
+    }
+    }
